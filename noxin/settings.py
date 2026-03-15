e8110931da290cfdc6776e81035ed3e6a8a7d095
Nikodem Wróbel <nixonsparrow@gmail.com>
2026-03-15 17:11:33 +0100

    Fix ALLOWED_HOSTS

--- noxin/settings.py
+++ noxin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-config("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 
 
 # Application definition
